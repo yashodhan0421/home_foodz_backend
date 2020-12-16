21169966ab7632cfed081dfec141a9a47f0040de
yashodhan0421 <yashodhankalia0@gmail.com>
2020-12-17 02:44:38 +0530

    added config files and package.json updates

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
 import express from 'express';
+import * as routes from './apis/routes';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+
+// home route
 app.get('/', (req, res) => {
   res.send('lets start from here');
 });
 
-app.get("*", (req, res) => {
-    res.json({status: "failed", message: "have you lost buddy"});
-})
+// all routes
+app.use(routes.auth);
 
+// 404
+app.get('*', (req, res) => {
+  res.json({ status: 'failed', message: 'have you lost buddy' });
+});
 
 app.listen(port, (err?: any) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`server started on port ${port} ...`);
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started on port ${port} ...`);
+  }
+});
